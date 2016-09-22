e8e08db0ef5524f4429ca4a7579878be05b7f4cd
david273 <david@davidnliz.com>
2016-09-22 16:58:23 -0500

    removed non core extensions

--- extensions/dojo-event/views/single-dojo-event.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) ) { die(); }
-
-get_header();
-
-include 'content-dojo-event.php';
-
-get_footer();
--- extensions/dojo-payment/views/webhook-instructions.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) { die(); }
-?>
-<p>Copy this url to your Stripe Account Settings -> Webhooks:<br>
-<input id="stripe_webhook_url" type="text" class="regular-text name="webhook_url" value="<?php echo esc_attr( $this->ajax( 'stripe_webhook' ) ) ?>" readonly>
-</p>
-<script>jQuery("#stripe_webhook_url").click(function() { jQuery(this).select(); } );</script>
-
