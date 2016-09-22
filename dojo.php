9a104436bea1556b686f4df69f85de5957cfc55d
david273 <david@davidnliz.com>
2016-09-22 16:56:13 -0500

    added more header info to plugin

--- dojo.php
+++ dojo.php
@@ -1,14 +1,27 @@
 <?php
-/**
- * @package Dojo 
- * @version 1.0
- */
 /*
 Plugin Name: Dojo
 Plugin URI: http://dojosource.com
 Description: Manage and grow your martial arts school with easy to use tools for your students, teachers and you!
-Author: David Mathis 
+Author: Dojo Source LLC
+Text Domain: dojo
+License: GPLv2 or later
 Version: 1.0
+*/
+
+/*
+Dojo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Dojo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Dojo. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 if ( ! defined( 'ABSPATH' ) ) { die(); }
